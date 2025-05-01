7682d81f15826e14f2646de166f74da9eec552ec
oleksandr <groupdchem.sales@gmail.com>
2025-05-01 20:23:31 +0300

    add initial files

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,8 @@
-export const PATH_DB = './src/db/db.json';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const PATH_DB = join(__dirname, '../db/db.json');
